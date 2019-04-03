f5f4deda54356c7733090bd4377a053e509aa381
Liyue1998 <1757759700@qq.com>
2019-04-03 14:17:23 +0800

    first雪梨作业

---
+++ src-作业/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import Counter from './components/Counter';
+import TodoList from './components/TodoList';
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>-
+               <Counter/>
+               <br/>
+               <TodoList/>
+            </div>
+
+        )
+    }
+}
